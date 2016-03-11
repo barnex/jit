7af1ddf3c7318b432fe377eeaa439de34a1a0801
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-03-11 22:35:30 +0000

    start synthesizing code

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"golang.org/x/sys/unix"
+	"bytes"
 	"os"
 )
 
@@ -13,17 +14,48 @@ import (
 import "C"
 import "unsafe"
 
-func main() {
-	code := []byte{
+var(
+	openFunc = []byte{
 		0x55,             // push   %rbp
 		0x48, 0x89, 0xe5, // mov    %rsp,%rbp
+	}
+	add10 = []byte{
 		0xf2, 0x0f, 0x58, 0xc1, // addsd  %xmm1,%xmm0
-		0x48, 0xb8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, 0x3f, // movabs $0x3ff0000000000000,%rax
+	}
+	movabs = []byte{
+		0x48, 0xb8, // movabs ...
+	}
+	abs0 = []byte{
 		0x66, 0x48, 0x0f, 0x6e, 0xc0, // movq   %rax,%xmm0
+	}
+	closeFunc = []byte{
 		0x5d, //  pop    %rbp
 		0xc3, // retq
 	}
-	mem, err := makeExecutable(code)
+)
+
+type codeBuffer struct{ bytes.Buffer }
+
+func(b *codeBuffer) emit(ops ...[]byte){
+	for _, op := range ops{
+		b.Write(op)
+	}
+}
+
+func main() {
+	//code := []byte{
+	//	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, 0x3f, // movabs $0x3ff0000000000000,%rax
+	//}
+
+	var code codeBuffer
+
+	code.emit(
+		openFunc,
+		add10,
+		closeFunc,
+	)
+
+	mem, err := makeExecutable(code.Bytes())
 	if err != nil {
 		fatal(err)
 	}
