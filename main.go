b2dee6d3080e87808235a082fd5704e150d76ec0
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-03-11 22:17:58 +0000

    64 arithmetic

--- main.go
+++ main.go
@@ -6,23 +6,30 @@ import (
 	"os"
 )
 
-//int run(void *code) {
-//  int (*func)() = code;
-//  return func();
+//double run(void *code, double x, double y) {
+//  double (*func)(double, double) = code;
+//  return func(x, y);
 //}
 import "C"
 import "unsafe"
 
 func main() {
-	// mov eax, $5
-	// ret
-	code := []byte{0xb8, 0x05, 0x00, 0x00, 0x00, 0xc3}
+	code := []byte{
+		0x55,             // push   %rbp
+		0x48, 0x89, 0xe5, // mov    %rsp,%rbp
+		0xf2, 0x0f, 0x58, 0xc1, // addsd  %xmm1,%xmm0
+		0x48, 0xb8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, 0x3f, // movabs $0x3ff0000000000000,%rax
+		0x66, 0x48, 0x0f, 0x6e, 0xc0, // movq   %rax,%xmm0
+		0x5d, //  pop    %rbp
+		0xc3, // retq
+	}
 	mem, err := makeExecutable(code)
 	if err != nil {
 		fatal(err)
 	}
 	defer unix.Munmap(mem)
-	fmt.Println(C.run(unsafe.Pointer(&mem[0])))
+	result := C.run(unsafe.Pointer(&mem[0]), 3, 40)
+	fmt.Println(result)
 }
 
 func makeExecutable(code []byte) ([]byte, error) {
