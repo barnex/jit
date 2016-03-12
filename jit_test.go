853da42c8efe62cfc9854342a8fc4fad37de72c1
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-03-12 15:49:29 +0000

    start unit tests

---
+++ jit_test.go
@@ -0,0 +1,30 @@
+package main
+
+import(
+"testing"
+)
+
+func TestJIT(t*testing.T){
+	tests := []struct{
+		expr string
+		x, y float64
+		want float64
+	}{
+		{"x", 42, 666, 42},
+		{"y", 42, 666, 666},
+	}
+
+	for _, test := range tests{
+		var b Buf
+		err := b.Compile(test.expr)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		have := b.call(test.x, test.y)
+		b.Free()
+		if have != test.want{
+				t.Errorf("%v with x=%v,y=%v: have %v, want: %v", test.expr, test.x, test.y, have, test.want)	
+		}
+	}
+}
--- main.go
+++ main.go
@@ -25,22 +25,3 @@ func main() {
 	}
 
 }
-
-//func main() {
-//	var code codeBuffer
-//
-//	code.emit(openFunc, mul10, closeFunc)
-//
-//	mem, err := makeExecutable(code.Bytes())
-//	if err != nil {
-//		fatal(err)
-//	}
-//	defer unix.Munmap(mem)
-//	result := call(mem, 3, 30)
-//	fmt.Println(result)
-//}
-
-func fatal(err error) {
-	fmt.Fprintln(os.Stderr, err)
-	os.Exit(1)
-}
