09eb31760e5b70e4cb03fd568f6ba04fc6bdb3c8
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-03-24 22:26:40 +0000

    cleanup

--- compiler.go
+++ compiler.go
@@ -13,11 +13,7 @@ package jit
 import (
 	"bytes"
 	"fmt"
-	"go/ast"
-	"go/token"
 	"os"
-	"reflect"
-	"strconv"
 )
 
 // buf accumulates machine code.
@@ -64,7 +60,7 @@ func Compile(expr string) (c *Code, e error) {
 	b.emit(sub_rsp(16))                      // stack space for x, y
 	b.emit(mov_xmm0_rax, mov_rax_x_rbp(-8))  // x on stack
 	b.emit(mov_xmm1_rax, mov_rax_x_rbp(-16)) // y on stack
-	root.compile(&b)                          // function body (jit code)
+	root.compile(&b)                         // function body (jit code)
 	b.emit(add_rsp(16))                      // free stack space for x,y
 	b.emit(pop_rbp, ret)                     // return from function
 
@@ -83,109 +79,6 @@ func (b *buf) emit(ops ...[]byte) {
 	for _, op := range ops {
 		b.Write(op)
 	}
-}
-
-// emitExpr compiles an expression and stores the machine code.
-func (b *buf) emitExpr(e ast.Expr) {
-	switch e := e.(type) {
-	default:
-		panic(err(e.Pos(), "syntax error:", typ(e)))
-	case *ast.Ident:
-		b.emitIdent(e)
-	case *ast.BasicLit:
-		b.emitBasicLit(e)
-	case *ast.BinaryExpr:
-		b.emitBinaryExpr(e)
-	case *ast.ParenExpr:
-		b.emitExpr(e.X)
-	//case *ast.UnaryExpr:
-	//	return w.compileUnaryExpr(e)
-	case *ast.CallExpr:
-		b.emitCall(e)
-	}
-}
-
-func (b *buf) emitCall(e *ast.CallExpr) {
-	name := e.Fun.(*ast.Ident).Name
-	fptr := funcs[name]
-	if fptr == 0 {
-		panic(err(e.Pos(), "undefined:", name))
-	}
-
-	if len(e.Args) != 1 {
-		panic(err(e.Pos(), "need one argument"))
-	}
-	b.emitExpr(e.Args[0])
-	b.emit(mov_uint_rax(fptr), call_rax)
-}
-
-// emitIdent compiles an identifier (x or y) and stores the machine code.
-func (b *buf) emitIdent(e *ast.Ident) {
-	switch e.Name {
-	default:
-		panic(err(e.Pos(), "undefined variable:", e.Name))
-	case "x":
-		b.emit(mov_x_rbp_rax(-8), mov_rax_xmm0)
-	case "y":
-		b.emit(mov_x_rbp_rax(-16), mov_rax_xmm0)
-	}
-}
-
-// emitBasicLit compiles a number literal, e.g. "2" and stores the machine code.
-func (b *buf) emitBasicLit(e *ast.BasicLit) {
-	switch e.Kind {
-	default:
-		panic(err(e.Pos(), "syntax error:", e.Value, "(", typ(e), ")"))
-	case token.FLOAT, token.INT:
-		v, err := strconv.ParseFloat(e.Value, 64)
-		if err != nil {
-			panic(err)
-		}
-		b.emit(mov_float_rax(v), mov_rax_xmm0)
-	}
-}
-
-// emitBinaryExpr compiles a binary expression, e.g. x+1, and stores the machine code.
-func (b *buf) emitBinaryExpr(n *ast.BinaryExpr) {
-	b.emitExpr(n.Y) // y in xmm0
-
-	// stash result
-	reg := b.allocReg()
-	if reg == -1 {
-		b.emit(mov_xmm0_rax, push_rax)
-	} else {
-		b.emit(mov_xmm(0, reg))
-	}
-
-	b.emitExpr(n.X) // x in xmm0
-	if reg == -1 {
-		b.emit(pop_rax, mov_rax_xmm1) // x in xmm1
-	} else {
-		b.emit(mov_xmm(reg, 1))
-	}
-	b.freeReg(reg)
-
-	switch n.Op {
-	default:
-		panic(err(n.Pos(), "syntax error:", n.Op))
-	case token.ADD:
-		b.emit(add_xmm1_xmm0)
-	case token.SUB:
-		b.emit(sub_xmm1_xmm0)
-	case token.MUL:
-		b.emit(mul_xmm1_xmm0)
-	case token.QUO:
-		b.emit(div_xmm1_xmm0)
-	}
-
-	// result in xmm0
-}
-
-func typ(x interface{}) reflect.Type {
-	return reflect.TypeOf(x)
-}
-func err(pos token.Pos, msg ...interface{}) error {
-	return fmt.Errorf("%v: %v", pos, fmt.Sprintln(msg...))
 }
 
 func (b *buf) dump(fname string) {
