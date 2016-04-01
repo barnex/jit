504808a4a481936fb91b2d9b9d640c547d820576
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-04-01 20:59:39 +0100

    switch and test for using call depth

--- compiler.go
+++ compiler.go
@@ -8,7 +8,125 @@ import (
 
 var (
 	useRegisters = true
+	useCallDepth = true
 )
+
+// Compile compiles an arithmetic expression, which may contain the variables x and y. E.g.:
+// 	(x+1) * (y-2)
+// If no longer needed, the returned code must be explicitly freed with Free().
+func Compile(ex string) (c *Code, e error) {
+	root, err := Parse(ex)
+	if err != nil {
+		return nil, err
+	}
+
+	b := buf{hasCall: make(map[expr]bool), callDepth: make(map[expr]int)}
+	recordCalls(root, b.hasCall)
+	if useCallDepth {
+		recordDepth(root, b.callDepth)
+	}
+
+	b.emit(push_rbp, mov_rsp_rbp)            // function preamble
+	b.emit(sub_rsp(16))                      // stack space for x, y
+	b.emit(mov_xmm0_rax, mov_rax_x_rbp(-8))  // x on stack
+	b.emit(mov_xmm1_rax, mov_rax_x_rbp(-16)) // y on stack
+	root.compile(&b)                         // function body (jit code)
+	b.emit(add_rsp(16))                      // free stack space for x,y
+	b.emit(pop_rbp, ret)                     // return from function
+
+	b.dump("b.out")
+	//fmt.Println(ex, ":", b.nRegistersHit, "reg hits,", b.maxReg, "highest register used, ", b.nStackSpill, "stack spills")
+
+	instr, err := makeExecutable(b.Bytes())
+	if err != nil {
+		return nil, err
+	}
+	return &Code{instr}, nil
+}
+
+// buf accumulates machine code.
+type buf struct {
+	bytes.Buffer
+	usedReg                            [8]bool
+	nRegistersHit, nStackSpill, maxReg int
+	hasCall                            map[expr]bool
+	callDepth                          map[expr]int
+}
+
+// emit writes machine code to the buffer.
+func (b *buf) emit(ops ...[]byte) {
+	for _, op := range ops {
+		b.Write(op)
+	}
+}
+
+func (b *buf) stash(destroyRegs bool) int {
+	// stash result
+	reg := -1
+	if !destroyRegs {
+		reg = b.allocReg()
+	} else {
+		b.nStackSpill++
+	}
+	if reg == -1 {
+		b.emit(mov_xmm0_rax, push_rax)
+	} else {
+		b.emit(mov_xmm(0, reg))
+	}
+	return reg
+}
+
+func (b *buf) unstash(reg, dest int) {
+	switch {
+	case reg == -1 && dest == 1:
+		b.emit(pop_rax, mov_rax_xmm1)
+	case reg == -1 && dest == 0:
+		b.emit(pop_rax, mov_rax_xmm0)
+	case reg != -1:
+		b.emit(mov_xmm(reg, dest))
+	default:
+		panic("bug")
+	}
+	b.freeReg(reg)
+}
+
+func (b *buf) allocReg() int {
+	if !useRegisters {
+		b.nStackSpill++
+		return -1
+	}
+	for i := 2; i < len(b.usedReg); i++ {
+		if !b.usedReg[i] {
+			b.usedReg[i] = true
+			b.nRegistersHit++
+			if i > b.maxReg {
+				b.maxReg = i
+			}
+			return i
+		}
+	}
+	b.nStackSpill++
+	return -1
+}
+
+func (b *buf) freeReg(reg int) {
+	if reg == -1 {
+		return
+	}
+	if !b.usedReg[reg] {
+		panic(fmt.Sprint("register double free", reg))
+	}
+	b.usedReg[reg] = false
+}
+
+func (b *buf) dump(fname string) {
+	f, err := os.Create(fname)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	f.Write(b.Bytes())
+}
 
 func (e *variable) compile(b *buf) {
 	switch e.name {
@@ -65,36 +183,6 @@ func (e *binexpr) compile(b *buf) {
 	}
 }
 
-func (b *buf) stash(destroyRegs bool) int {
-	// stash result
-	reg := -1
-	if !destroyRegs {
-		reg = b.allocReg()
-	} else {
-		b.nStackSpill++
-	}
-	if reg == -1 {
-		b.emit(mov_xmm0_rax, push_rax)
-	} else {
-		b.emit(mov_xmm(0, reg))
-	}
-	return reg
-}
-
-func (b *buf) unstash(reg, dest int) {
-	switch {
-	case reg == -1 && dest == 1:
-		b.emit(pop_rax, mov_rax_xmm1)
-	case reg == -1 && dest == 0:
-		b.emit(pop_rax, mov_rax_xmm0)
-	case reg != -1:
-		b.emit(mov_xmm(reg, dest))
-	default:
-		panic("bug")
-	}
-	b.freeReg(reg)
-}
-
 func (e *callexpr) compile(b *buf) {
 	if len(e.args) != 1 {
 		panic(fmt.Sprintf("%v arguments not supported", len(e.args)))
@@ -106,89 +194,4 @@ func (e *callexpr) compile(b *buf) {
 
 	e.args[0].compile(b)
 	b.emit(mov_uint_rax(fptr), call_rax)
-}
-
-// buf accumulates machine code.
-type buf struct {
-	bytes.Buffer
-	usedReg                            [8]bool
-	nRegistersHit, nStackSpill, maxReg int
-	hasCall                            map[expr]bool
-	callDepth                          map[expr]int
-}
-
-func (b *buf) allocReg() int {
-	if !useRegisters {
-		b.nStackSpill++
-		return -1
-	}
-	for i := 2; i < len(b.usedReg); i++ {
-		if !b.usedReg[i] {
-			b.usedReg[i] = true
-			b.nRegistersHit++
-			if i > b.maxReg {
-				b.maxReg = i
-			}
-			return i
-		}
-	}
-	b.nStackSpill++
-	return -1
-}
-
-func (b *buf) freeReg(reg int) {
-	if reg == -1 {
-		return
-	}
-	if !b.usedReg[reg] {
-		panic(fmt.Sprint("register double free", reg))
-	}
-	b.usedReg[reg] = false
-}
-
-// Compile compiles an arithmetic expression, which may contain the variables x and y. E.g.:
-// 	(x+1) * (y-2)
-// If no longer needed, the returned code must be explicitly freed with Free().
-func Compile(ex string) (c *Code, e error) {
-	root, err := Parse(ex)
-	if err != nil {
-		return nil, err
-	}
-
-	b := buf{hasCall: make(map[expr]bool), callDepth: make(map[expr]int)}
-	recordCalls(root, b.hasCall)
-	recordDepth(root, b.callDepth)
-
-	b.emit(push_rbp, mov_rsp_rbp)            // function preamble
-	b.emit(sub_rsp(16))                      // stack space for x, y
-	b.emit(mov_xmm0_rax, mov_rax_x_rbp(-8))  // x on stack
-	b.emit(mov_xmm1_rax, mov_rax_x_rbp(-16)) // y on stack
-	root.compile(&b)                         // function body (jit code)
-	b.emit(add_rsp(16))                      // free stack space for x,y
-	b.emit(pop_rbp, ret)                     // return from function
-
-	b.dump("b.out")
-	//fmt.Println(ex, ":", b.nRegistersHit, "reg hits,", b.maxReg, "highest register used, ", b.nStackSpill, "stack spills")
-
-	instr, err := makeExecutable(b.Bytes())
-	if err != nil {
-		return nil, err
-	}
-	return &Code{instr}, nil
-}
-
-// emit writes machine code to the buffer.
-func (b *buf) emit(ops ...[]byte) {
-	for _, op := range ops {
-		b.Write(op)
-	}
-}
-
-func (b *buf) dump(fname string) {
-	f, err := os.Create(fname)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	f.Write(b.Bytes())
 }
