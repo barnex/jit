a75750a0d0d73c17f16a64627bf6747b1ac88666
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-04-01 22:35:50 +0100

    readme.

--- benchmark_test.go
+++ benchmark_test.go
@@ -3,9 +3,21 @@ package jit
 import "testing"
 
 const (
-	nx = 500
-	ny = 500
+	nx = 1000
+	ny = 1000
 )
+
+func BenchmarkCompileSmall(b *testing.B) {
+	for i:=0; i<b.N; i++{
+	Compile("(x+y)*2 + (1+x) / y")
+	}
+}
+
+func BenchmarkCompileBig(b *testing.B) {
+	for i:=0; i<b.N; i++{
+		Compile("1+x+(3+y*4+((((x+y*2)+x)+sqrt(8))+y)+10*sin(2-x+y/3))+11")
+	}
+}
 
 func BenchmarkJITSmall(b *testing.B) {
 	code, err := Compile("(x+y)*2 + (1+x) / y")
